9ad0f3437d852b3d09f6d3bb6c4cc44763cace52
Sam Soffes <sam@soff.es>
2015-08-12 15:28:35 -0700

    Fix tests

--- SyntaxKit/Tests/TestHelper.swift
+++ SyntaxKit/Tests/TestHelper.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import XCTest
+import X
 @testable import SyntaxKit
 
 func yamlLanguage() -> Language! {
--- SyntaxKit/Tests/ThemeTests.swift
+++ SyntaxKit/Tests/ThemeTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import X
 @testable import SyntaxKit
 
 class ThemeTests: XCTestCase {
